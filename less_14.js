da91514a4f7ad2786527a0000da94b18baaf060f
VictorLivovich <vitTig@online.ua>
2023-06-15 17:53:21 +0300

    text

---
+++ less_14.js
@@ -0,0 +1,12 @@
+new Swiper(".sport__slider",{
+   pagination: {
+      el:".swiper-pagination",
+      clickable:true,
+   },
+});
+new Swiper(".sport__slider2",{
+   pagination: {
+      el:".swiper-pagination",
+      clickable:true,
+   },
+});
